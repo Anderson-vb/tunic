80b6390f69a2a4e4907c15c727864261a39e8941
Andersonvb <andersonvalenciabermudez@gmail.com>
2022-10-16 10:26:10 -0500

    Add server with Express

---
+++ website/src/app.js
@@ -0,0 +1,98 @@
+let posts = [];
+
+const createPostBtn = document.querySelector('#create-post-btn');
+
+const addPost = (title, content) => {
+  let maxId = 0;
+  if (posts.length != 0) {
+    const ids = posts.map(post => post.id);
+    maxId = Math.max(...ids);
+  }
+  
+  const post = {
+    id: maxId + 1,
+    title: title,
+    content: content,
+  }
+
+  fetch('http://localhost:3000/', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(post),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data);
+    posts = data;
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+const createPostElement = (id, title, content) => {
+
+  const postDiv = document.createElement('div');
+  const post__imgDiv = document.createElement('div');
+  const post__containerDiv = document.createElement('div');
+  const post__titleDiv = document.createElement('div');
+  const post__contentDiv = document.createElement('div');
+  const img = document.createElement('img');
+  const hr = document.createElement('hr');
+
+  postDiv.classList.add('post');
+  post__imgDiv.classList.add('post__img');
+  post__containerDiv.classList.add('post__container');
+  post__titleDiv.classList.add('post__title');
+  post__contentDiv.classList.add('post__content');
+  img.setAttribute('src', '../images/forum/avatar.png');
+  img.setAttribute('alt', 'User');
+
+  post__titleDiv.innerHTML = title;
+  post__contentDiv.innerHTML = content;
+
+  postDiv.appendChild(post__imgDiv);
+  postDiv.appendChild(post__containerDiv);
+  post__imgDiv.appendChild(img);
+  post__containerDiv.appendChild(post__titleDiv);
+  post__containerDiv.appendChild(post__contentDiv);
+  post__containerDiv.appendChild(hr);
+
+  const postsSection = document.querySelector('.post-content-section');
+  postsSection.appendChild(postDiv);
+
+};
+
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
+
+const renderAllPosts = () => {
+  removeAllChildNodes(document.querySelector('.post-content-section'));
+
+  fetch('http://localhost:3000')
+    .then(res => res.json())
+    .then(data => {
+      posts = data;
+      postElements = posts.map(post => {
+        return createPostElement(post.id, post.title, post.content, post.img);
+      });
+    })
+};
+
+createPostBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const titleInput = document.querySelector('#title-input');
+  const contentInput = document.querySelector('#content-input');
+
+  addPost(titleInput.value, contentInput.value);
+  renderAllPosts();
+  titleInput.value = '';
+  contentInput.value = '';
+});
+
+window.onload = renderAllPosts;
